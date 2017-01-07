0252513eb728ca3f5f47f3bebf6b6ea25c80cab5
Oliver Kowalke <oliver.kowalke@gmx.de>
2017-01-07 19:37:40 +0100

    changed callcc

---
+++ code/fibonacci.cpp
@@ -0,0 +1,21 @@
+std::continuation c2=
+    std::callcc(  // (a)
+        [](std::continuation && c1){
+            int a=0;
+            int b=1;
+            for(;;){
+                c=std::resume(std::move(c1),a); // (b)
+                int next=a+b;
+                a=b;
+                b=next;
+            }
+            return std::move(c1);
+        });
+for (int j=0;j<10;++j) {
+    int i=std::transfer_data<int>(c2); // (c)
+    std::cout << i << " ";
+    c2=std::resume(std::move(c2)); // (d)
+}
+
+output:
+    0 1 1 2 3 5 8 13 21 34 55
--- code/loop.cpp
+++ code/loop.cpp
@@ -1,19 +1,19 @@
-std::continuation foo( std::continuation && c1) {
-    while ( c1) {
-        std::cout << "foo\n"; // (b)
-        c1 = // (f)
-            std::resume( std::move( c1) ); // (c)
+std::continuation foo(std::continuation && c1) {
+    while (c1) {
+        std::cout << "foo\n";
+        c1= // (e)
+           std::resume(std::move(c1)); // (b)
     }
-    return std::move( c1);
+    return std::move(c1);
 }
 
 int main() {
-    std::continuation c2 = // (d)
-        std::callcc( foo); // (a)
-    while ( c2) {
+    std::continuation c2= // (c)
+                         std::callcc(foo); // (a)
+    while (c2) {
         std::cout << "bar\n";
-        c2 = // (g)
-            std::resume( std::move( c2) ); // (e)
+        c2= // (f)
+           std::resume(std::move(c2)); // (d)
     }
     std::cout << "main: done" << std::endl;
     return 0;
@@ -22,5 +22,4 @@ int main() {
 output:
     foo
     bar
-    foo
     ...
