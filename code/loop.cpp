3d9d0ba974836428d1e119aef1bff74617f2482a
Oliver Kowalke <oliver.kowalke@gmx.de>
2017-01-29 07:51:47 +0100

    remove main() form example code

--- code/loop.cpp
+++ code/loop.cpp
@@ -7,16 +7,12 @@ std::continuation foo(std::continuation && c1) {
     return std::move(c1);
 }
 
-int main() {
-    std::continuation c2= // (c)
-                         std::callcc(foo); // (a)
-    while (c2) {
-        std::cout << "bar\n";
-        c2= // (f)
-           std::resume(std::move(c2)); // (d)
-    }
-    std::cout << "main: done" << std::endl;
-    return 0;
+std::continuation c2= // (c)
+                     std::callcc(foo); // (a)
+while (c2) {
+    std::cout << "bar\n";
+    c2= // (f)
+       std::resume(std::move(c2)); // (d)
 }
 
 output:
