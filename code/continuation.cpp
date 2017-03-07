147d5402cb465b62310561aff48bcc215473e17a
Oliver Kowalke <oliver.kowalke@gmail.com>
2017-03-07 19:07:59 +0100

    stack destruction

--- code/continuation.cpp
+++ code/continuation.cpp
@@ -32,3 +32,5 @@ continuation callcc( Fn &&, Arg ...);
 
 template< typename StackAlloc, typename Fn, typename ...Arg >
 continuation callcc( std::allocator_arg_t, StackAlloc, Fn &&, Arg ...);
+
+struct unwind_exception {};
