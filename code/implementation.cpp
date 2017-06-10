733a331651a92d25e91499d38fc3c9953200721e
Nat Goodspeed <nat@lindenlab.com>
2017-06-10 15:44:44 -0400

    Editorial pass over first P0534R1 draft

--- code/implementation.cpp
+++ code/implementation.cpp
@@ -9,14 +9,14 @@ private:
 public:
     ~continuation() {
         if ( nullptr != vp) {
-            // resume context by calling resume_with()
+            // resume context by calling resume_with().
             // apply a special function to resume_with() 
-            // that throws a std::unwind_exception
-            // this exception is catched at the last stack-frame,
-            // e.g. the stack-frame that was prepared by callcc() before
-            // from this stack frame switch back to the context that has
-            // invoked ~continuation()
-            // deallocate the stack
+            // that throws a std::unwind_exception.
+            // this exception is caught at the last stack-frame,
+            // i.e. the stack-frame originally prepared by callcc().
+            // from that stack frame switch back to the context that
+            // invoked ~continuation().
+            // deallocate the stack.
         }
     }
 
@@ -32,7 +32,7 @@ public:
         // PROLOGUE (+ store instruction-pointer)
         // exchange current stack with `vp` (becomes current stack)
         // EPILOGUE (+ restore instruction-pointer)
-        // `fn` is loaded into instruction-pointer == `fn()` is executed by
+        // `fn` is loaded into instruction-pointer: `fn()` is executed by
         // the resumed execution context
         // stack-pointer of caller passed as std::continuation to `fn()`
     }
@@ -40,7 +40,7 @@ public:
 
 template< typename StackAlloc, typename Fn >
 continuation callcc( std::allocator_arg_t, StackAlloc salloc, Fn && fn) {
-    // allocated memory for stack `vp`
+    // allocate memory for stack `vp`
     // prepare stack (generate special stack-frame)
     // construct std::continuation from stack `vp` and resume it
     return continuation{ vp }.resume();
